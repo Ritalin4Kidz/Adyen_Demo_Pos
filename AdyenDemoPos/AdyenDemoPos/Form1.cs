5c2497768641eac8b9c95440ece661339e3d9ada
Callum Hands <callum@pceftpos.com.au>
2020-12-09 11:05:15 +1100

    DemoPOS Update

--- AdyenDemoPos/AdyenDemoPos/Form1.cs
+++ AdyenDemoPos/AdyenDemoPos/Form1.cs
@@ -15,6 +15,7 @@ namespace AdyenDemoPos
     public partial class Form1 : Form
     {
         const string API_KEY = "AQE1hmfxK4LHaxRLw0m/n3Q5qf3Ve55dHZxYTFdTxWq+l3JOk8J4BIF6xrL+9hM035r7qNCLaPMQwV1bDb7kfNy1WIxIIkxgBw==-lyFikbVyg+HZD2y+GgAGF+eB2YDq+VqCh5vLIkfUMS8=-A2paXM3Ta9wWTG6t";
+        const string API_ENDPOINT = "https://terminal-api-test.adyen.com/sync";
         public Form1()
         {
             InitializeComponent();
@@ -24,27 +25,148 @@ namespace AdyenDemoPos
         {
 
         }
+        /*
+          "CardAcquisitionRequest":{
+            "SaleData":{
+                "SaleTransactionID":{
+                    "TransactionID":"02072",
+                    "TimeStamp":"2020-01-07T14:14:04+00:00"
+                },
+                "TokenRequestedType":"Customer"
+            },
+            "CardAcquisitionTransaction":{
+                "TotalAmount":24.98
+            }
+        }
+         *
+         */
+        private void DoQuery()
+        {
+            var queryRequest = new Adyen.Model.Nexo.CardAcquisitionRequest
+            {
+                SaleData = new Adyen.Model.Nexo.SaleData
+                {
+                    SaleTransactionID = new Adyen.Model.Nexo.TransactionIdentification
+                    {
+                        TransactionID = "02072",
+                        TimeStamp = DateTime.Now
+                    },
+                    TokenRequestedType = Adyen.Model.Nexo.TokenRequestedType.Customer
+                },
+                CardAcquisitionTransaction = new Adyen.Model.Nexo.CardAcquisitionTransaction
+                {
+                    TotalAmount = 24.98M
+                }
+            };
+            //var amount = new Adyen.Model.Checkout.Amount("AUD", 99);
+            var POIRequest = new Adyen.Model.Nexo.SaleToPOIMessage
+            {
+                MessageHeader = new Adyen.Model.Nexo.MessageHeader
+                {
+                    MessageClass = Adyen.Model.Nexo.MessageClassType.Service,
+                    MessageCategory = Adyen.Model.Nexo.MessageCategoryType.CardAcquisition,
+                    MessageType = Adyen.Model.Nexo.MessageType.Request,
+                    ServiceID = "1020711110",
+                    SaleID = "POSSystemID12345",
+                    POIID = "V400m-346190779"
+                },
+                MessagePayload = queryRequest
+            };
+            var client = new Client(API_KEY, Adyen.Model.Enum.Environment.Test);
+            var checkout = new Adyen.Service.PosPaymentCloudApi(client);
+            try
+            {
+                var QueryResponse = checkout.TerminalApiCloudSync(POIRequest);
+                Receipt.Text = QueryResponse.MessagePayload.ToString();
+            }
+            catch (Exception e)
+            {
+                Receipt.Text = e.StackTrace;
+            }
+        }
+
+        private void DoCancel()
+        {
+            //var amount = new Adyen.Model.Checkout.Amount("AUD", 99);
+            var abortRequest = new Adyen.Model.Nexo.AbortRequest
+            {
+                AbortReason = "MerchantAbort",
+                MessageReference = new Adyen.Model.Nexo.MessageReference
+                {
+                    MessageCategory = Adyen.Model.Nexo.MessageCategoryType.Payment,
+                    SaleID = "POSSystemID12345",
+                    ServiceID = "21796"
+                }
+            };
+            //var amount = new Adyen.Model.Checkout.Amount("AUD", 99);
+            var POIRequest = new Adyen.Model.Nexo.SaleToPOIMessage
+            {
+                MessageHeader = new Adyen.Model.Nexo.MessageHeader
+                {
+                    MessageClass = Adyen.Model.Nexo.MessageClassType.Service,
+                    MessageCategory = Adyen.Model.Nexo.MessageCategoryType.CardAcquisition,
+                    MessageType = Adyen.Model.Nexo.MessageType.Request,
+                    ServiceID = "1020711110",
+                    SaleID = "POSSystemID12345",
+                    POIID = "V400m-346190779"
+                },
+                MessagePayload = abortRequest
+            };
+            var client = new Client(API_KEY, Adyen.Model.Enum.Environment.Test);
+            var checkout = new Adyen.Service.PosPaymentCloudApi(client);
+            try
+            {
+                var AbortResponse = checkout.TerminalApiCloudSync(POIRequest);
+                Receipt.Text = AbortResponse.MessagePayload.ToString();
+            }
+            catch (Exception e)
+            {
+                Receipt.Text = e.StackTrace;
+            }
+
+        }
 
         private void DoTransaction()
         {
             // Create a paymentsRequest
-            var amount = new Adyen.Model.Checkout.Amount("AUD", 99);
-            var paymentsRequest = new Adyen.Model.Checkout.PaymentRequest
+            var paymentsRequest = new Adyen.Model.Nexo.PaymentRequest
             {
-                Reference = "343457546845",
-                Amount = amount,
-                ReturnUrl = @"ws_489019@Company.SupportRecruitement",
-                MerchantAccount = "RecruitementCafe"
+                SaleData = new Adyen.Model.Nexo.SaleData
+                {
+                    SaleTransactionID = new Adyen.Model.Nexo.TransactionIdentification
+                    {
+                        TransactionID = "02072",
+                        TimeStamp = DateTime.Now
+                    },
+                },
+                PaymentTransaction = new Adyen.Model.Nexo.PaymentTransaction
+                {
+                    AmountsReq = new Adyen.Model.Nexo.AmountsReq
+                    {
+                        Currency = "AUD",
+                        RequestedAmount = 25.00M
+                    }
+                }
             };
-            paymentsRequest.AddCardData("4111111111111111", "10", "2020", "737", "John Smith");
-
-            //Create the http client
-            var client = new Client(API_KEY, Adyen.Model.Enum.Environment.Test);//or Model.Enum.Environment.Live
-            var checkout = new Checkout(client);
-            //Make the call to the service. This example code makes a call to /payments
+            var POIRequest = new Adyen.Model.Nexo.SaleToPOIMessage
+            {
+                MessageHeader = new Adyen.Model.Nexo.MessageHeader
+                {
+                    MessageClass = Adyen.Model.Nexo.MessageClassType.Service,
+                    MessageCategory = Adyen.Model.Nexo.MessageCategoryType.CardAcquisition,
+                    MessageType = Adyen.Model.Nexo.MessageType.Request,
+                    ServiceID = "1020711110",
+                    SaleID = "POSSystemID12345",
+                    POIID = "V400m-346190779"
+                },
+                MessagePayload = paymentsRequest
+            };
+            var client = new Client(API_KEY, Adyen.Model.Enum.Environment.Test);
+            var checkout = new Adyen.Service.PosPaymentCloudApi(client);
             try
             {
-                var paymentsResponse = checkout.Payments(paymentsRequest);
+                var PaymentResponse = checkout.TerminalApiCloudSync(POIRequest);
+                Receipt.Text = PaymentResponse.MessagePayload.ToString();
             }
             catch (Exception e)
             {
@@ -59,12 +181,12 @@ namespace AdyenDemoPos
 
         private void Query_Click(object sender, EventArgs e)
         {
-
+            DoQuery();
         }
 
         private void Cancel_Click(object sender, EventArgs e)
         {
-
+            DoCancel();
         }
     }
 }
